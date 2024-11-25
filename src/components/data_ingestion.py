5b309d04aa1caea2f7a3a8589bb4e1feeafb5dd7
coder7475 <robiulhossain7475@gmail.com>
2024-11-25 09:35:33 +0600

    start: EDA, data transformation and feature engineering

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from src.logger import logging
 from src.exception import CustomException
-# from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation
 # from src.components.model_trainer import ModelTrainer
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
@@ -48,8 +48,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data_path, test_data_path = obj.initiate_data_ingestion()
 
-    # data_transformation = DataTransformation()
-    # train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
     # modeltrainer = ModelTrainer()
     # print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
